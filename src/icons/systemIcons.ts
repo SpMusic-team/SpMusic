83a2e963a510b917c30fb1d2e2c7ae48e7b0531e
戴龙星 <124897400+DraDai@users.noreply.github.com>
2026-07-13 01:58:05 +0800

    feat: 新增图标提供者默认实现

--- src/icons/systemIcons.ts
+++ src/icons/systemIcons.ts
@@ -1,13 +1,15 @@
+import { defaultSystemIcons } from '@/icons/providers/defaultIcons'
 import { fluentSystemIcons } from '@/icons/providers/fluentIcons'
 import { iosSystemIcons } from '@/icons/providers/iosIcons'
 
 export type { SystemIcon, SystemIconProvider } from '@/icons/types'
 
 export const iconProviders = {
+  default: defaultSystemIcons,
   ios: iosSystemIcons,
   fluent: fluentSystemIcons,
-  windows: fluentSystemIcons,
+  windows: defaultSystemIcons,
   fallback: fluentSystemIcons,
 } as const
 
-export const systemIcons = iconProviders.windows
+export const systemIcons = iconProviders.default
